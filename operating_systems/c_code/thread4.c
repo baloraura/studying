e6da41210a1900e2f9f5c709ab34c04d13ec4a5c
Luke Gearty <lukegearty@gmail.com>
2025-06-06 16:36:46 +0000

    messed around with some thread studying in c_code

--- operating_systems/c_code/thread4.c
+++ operating_systems/c_code/thread4.c
@@ -3,9 +3,48 @@
 #include <stdlib.h>
 
 
+#define THREADS 5
+
+void* thread_function(void *arg) {
+	int thread_id = *(int*)arg;
+	printf("Thread %d: Started\n", thread_id);
+
+	for (int i = 0; i < 5; i++) {
+		printf("Thread %d: Process %d\n", thread_id, i);
+	}
+
+	printf("Thread %d: Finished\n", thread_id);
+	return NULL;
+}
 
 
 int main() {
+
+	pthread_t threads[THREADS];
+
+	int thread_ids[THREADS];
+
+	for (int i = 0; i < THREADS; i++) {
+		thread_ids[i] = i + 1;
+	}
+
+	int num_threads = 2;
+
+	for (int i = 0; i < num_threads; i++) {
+		if (pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]) != 0) {
+			perror("Failed to create thread\n");
+			return 1;
+		}
+	}
+
+	for (int i = 0; i < num_threads; i++) {
+		if (pthread_join(threads[i], NULL) != 0) {
+			perror("Failed to join thread\n");
+			return 1;
+		}
+	}
+
+	printf("Main Thread: ALL threads completed\n");
 	
 	return 0;
 }
